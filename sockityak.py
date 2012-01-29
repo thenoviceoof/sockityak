2b933789547a4177d93fbe8d1c88530fed66228c
Nathan Hwang <beta.entity.k@gmail.com>
2012-01-29 07:47:29 -0500

    Converted part of the chat mechanism to redis pubsub
    Reason to do so is that if there are every multiple servers, then
    the updates will stay local: going through the pubsub system
    means that the updates will go to all the tornado instances.

--- sockityak.py
+++ sockityak.py
@@ -8,6 +8,8 @@ import tornado.websocket
 
 import redis
 
+import threading
+
 import json
 import os
 from operator import itemgetter
@@ -31,11 +33,33 @@ class ChannelHandler(tornado.web.RequestHandler):
         posts = [json.loads(p) for p in r.lrange(key, -10, -1)]
         self.render("templates/channel.html", posts=posts, channel=channel)
 
+################################################################################
+# websockets stuff
+
+# connection pool
 connections = {}
+
+# pubsub listening thread
+def pubsub_listen():
+    r = redis.Redis(host="localhost",port=6379,db=0)
+    p = r.pubsub()
+    p.psubscribe("pub:*")
+    g = p.listen()
+    # waiting for published data
+    d = g.next()
+    while d:
+        channel = d["channel"].split(":")[1]
+        data = d["data"]
+        for conn in connections[channel]:
+            conn.send_msg(data)
+        d = g.next()
+
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self, channel):
-        print "WebSocket opened"
+        print("WebSocket opened")
         self.channel = channel
+
+        # add to connections dict
         conns = connections.setdefault(channel, set())
         conns.add(self)
         self.conns = conns
@@ -50,8 +74,9 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
         j = json.dumps({"m":message, "u":"User", "i":length-1})
         r.lset(key, length-1, j)
 
-        for conn in self.conns:
-            conn.send_msg(j)
+        # publish
+        pub_channel = "pub:%s" % self.channel
+        r.publish(pub_channel, message)
 
     def send_msg(self, msg):
         self.write_message(msg)
@@ -66,6 +91,9 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
             except:
                 pass # don't do anything, probably a race
 
+################################################################################
+# infrastructure
+
 handlers = [
     (r"/", MainHandler),
     (r"/channel/(\w+)", ChannelHandler),
@@ -79,5 +107,8 @@ settings = dict(
 application = tornado.web.Application(handlers, **settings)
 
 if __name__ == "__main__":
+    # start pubsub listening thread
+    threading.Thread(target=pubsub_listen).start()
+    # start tornado things
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
